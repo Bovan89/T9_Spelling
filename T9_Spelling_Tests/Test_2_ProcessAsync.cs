36f6d54230c1ff6d43ce328ba3cfc086298805b8
Bovan89 <53613707+Bovan89@users.noreply.github.com>
2019-12-06 11:58:35 +0500

    4

--- T9_Spelling_Tests/Test_2_ProcessAsync.cs
+++ T9_Spelling_Tests/Test_2_ProcessAsync.cs
@@ -28,5 +28,23 @@ namespace T9_Spelling_Tests
 
             Assert.That(res, Is.EqualTo("2 22 2223 33 3334 44 4445 55 5556 66 6667 77 777 77778 88 8889 99 999 9999"));
         }
+
+        [Test, Timeout(2000)]
+        public void ManyChars()
+        {
+            ParseT9 parseT9 = new ParseT9();
+            string res = parseT9.ProcessAsync("abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz ").Result;
+
+            Assert.That(res, Is.EqualTo("2 22 2223 33 3334 44 4445 55 5556 66 6667 77 777 77778 88 8889 99 999 999902 22 2223 33 3334 44 4445 55 5556 66 6667 77 777 77778 88 8889 99 999 999902 22 2223 33 3334 44 4445 55 5556 66 6667 77 777 77778 88 8889 99 999 999902 22 2223 33 3334 44 4445 55 5556 66 6667 77 777 77778 88 8889 99 999 999902 22 2223 33 3334 44 4445 55 5556 66 6667 77 777 77778 88 8889 99 999 999902 22 2223 33 3334 44 4445 55 5556 66 6667 77 777 77778 88 8889 99 999 999902 22 2223 33 3334 44 4445 55 5556 66 6667 77 777 77778 88 8889 99 999 999902 22 2223 33 3334 44 4445 55 5556 66 6667 77 777 77778 88 8889 99 999 999902 22 2223 33 3334 44 4445 55 5556 66 6667 77 777 77778 88 8889 99 999 999902 22 2223 33 3334 44 4445 55 5556 66 6667 77 777 77778 88 8889 99 999 999902 22 2223 33 3334 44 4445 55 5556 66 6667 77 777 77778 88 8889 99 999 999902 22 2223 33 3334 44 4445 55 5556 66 6667 77 777 77778 88 8889 99 999 999902 22 2223 33 3334 44 4445 55 5556 66 6667 77 777 77778 88 8889 99 999 999902 22 2223 33 3334 44 4445 55 5556 66 6667 77 777 77778 88 8889 99 999 999902 22 2223 33 3334 44 4445 55 5556 66 6667 77 777 77778 88 8889 99 999 999902 22 2223 33 3334 44 4445 55 5556 66 6667 77 777 77778 88 8889 99 999 999902 22 2223 33 3334 44 4445 55 5556 66 6667 77 777 77778 88 8889 99 999 999902 22 2223 33 3334 44 4445 55 5556 66 6667 77 777 77778 88 8889 99 999 999902 22 2223 33 3334 44 4445 55 5556 66 6667 77 777 77778 88 8889 99 999 999902 22 2223 33 3334 44 4445 55 5556 66 6667 77 777 77778 88 8889 99 999 999902 22 2223 33 3334 44 4445 55 5556 66 6667 77 777 77778 88 8889 99 999 999902 22 2223 33 3334 44 4445 55 5556 66 6667 77 777 77778 88 8889 99 999 99990"));
+        }
+
+        [Test]
+        public void WithPrefix()
+        {
+            ParseT9 parseT9 = new ParseT9();
+            string res = parseT9.ProcessAsync("abc", 1).Result;
+
+            Assert.That(res, Is.EqualTo(StaticData.StartString + "1" + StaticData.MiddleString + "2 22 222"));
+        }
     }
 }
